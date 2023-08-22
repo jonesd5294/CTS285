62530b31f4c2c8d89174b387bdcede0f7adc2e7b
jonesd5294 <99271180+jonesd5294@users.noreply.github.com>
2023-08-22 11:46:04 -0400

    Add files via upload

---
+++ M1T1_Jones/Console/Program.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * CTS 285
+ * M1T1
+ * David Jones
+ * 8/22/2023
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("David Jones");
+            Console.WriteLine("I am studying Java and C# this semester.");
+            Console.WriteLine("I'm retired from the army.  I'm looking to gain a new skill and further my education.");
+            Console.ReadLine();
+        }
+    }
+}
